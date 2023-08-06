20d855aec0c4047b61c4a95e9e97c6430ebe1888
wondu shiferaw <33760699+wondus36@users.noreply.github.com>
2023-08-06 21:59:39 +0300

    Add files via upload

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -8,4 +8,13 @@ document.ready(function () {
 		}
 		$(".amenities h4").text(Object.keys(amenities).sort().join(", "));
 	});
+
+	// get status of API
+	$.getJSON("http://0.0.0.0:5001/api/v1/status/", (data) => {
+		if (data.status === "OK") {
+			$("div#api_status").addClass("available");
+		} else {
+			$("div#api_status").removeClass("available");
+		}
+	});
 });
